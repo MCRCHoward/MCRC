a3c818ce990bfdec3e4f4396e4de27c3f4520dd4
DVitsme <derrick.valentine@gmail.com>
2026-02-09 13:55:38 -0500

    Refactor Tailwind configuration for improved readability and maintainability
    Reorganize the Tailwind configuration in tailwind.config.mjs by adjusting indentation and formatting for better clarity. This update maintains existing functionality while enhancing the overall structure of the configuration file.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -28,177 +28,181 @@ const config = {
     'bg-warning/30',
   ],
   theme: {
-    container: {
-      center: true,
-      padding: {
-        '2xl': '2rem',
-        DEFAULT: '1rem',
-        lg: '2rem',
-        md: '2rem',
-        sm: '1rem',
-        xl: '2rem',
-      },
-      screens: {
-        '2xl': '86rem',
-        lg: '64rem',
-        md: '48rem',
-        sm: '40rem',
-        xl: '80rem',
-      },
-    },
-    extend: {
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-      },
-      colors: {
-        border: 'var(--border)',
-        input: 'var(--input)',
-        ring: 'var(--ring)',
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        primary: {
-          DEFAULT: 'var(--primary)',
-          foreground: 'var(--primary-foreground)',
-        },
-        secondary: {
-          DEFAULT: 'var(--secondary)',
-          foreground: 'var(--secondary-foreground)',
-        },
-        tertiary: {
-          DEFAULT: 'var(--tertiary)',
-          foreground: 'var(--tertiary-foreground)',
-        },
-        yellow: {
-          DEFAULT: 'var(--yellow)',
-          foreground: 'var(--yellow-foreground)',
-        },
-        darkyellow: {
-          DEFAULT: 'var(--darkyellow)',
-          foreground: 'var(--darkyellow-foreground)',
-        },
-        purple: {
-          DEFAULT: 'var(--purple)',
-          foreground: 'var(--purple-foreground)',
-        },
-        blue: {
-          DEFAULT: 'var(--blue)',
-          foreground: 'var(--blue-foreground)',
-        },
-        darkbrown: {
-          DEFAULT: 'var(--darkbrown)',
-          foreground: 'var(--darkbrown-foreground)',
-        },
-        darkgreen: {
-          DEFAULT: 'var(--darkgreen)',
-          foreground: 'var(--darkgreen-foreground)',
-        },
-        destructive: {
-          DEFAULT: 'var(--destructive)',
-          foreground: 'var(--destructive-foreground)',
-        },
-        muted: {
-          DEFAULT: 'var(--muted)',
-          foreground: 'var(--muted-foreground)',
-        },
-        accent: {
-          DEFAULT: 'var(--accent)',
-          foreground: 'var(--accent-foreground)',
-        },
-        popover: {
-          DEFAULT: 'var(--popover)',
-          foreground: 'var(--popover-foreground)',
-        },
-        card: {
-          DEFAULT: 'var(--card)',
-          foreground: 'var(--card-foreground)',
-        },
-        success: {
-          DEFAULT: 'var(--success)',
-          foreground: 'var(--success-foreground)',
-        },
-        warning: 'var(--warning)',
-        error: 'var(--error)',
-        'chart-1': 'var(--chart-1)',
-        'chart-2': 'var(--chart-2)',
-        'chart-3': 'var(--chart-3)',
-        'chart-4': 'var(--chart-4)',
-        'chart-5': 'var(--chart-5)',
-        sidebar: {
-          DEFAULT: 'hsl(var(--sidebar-background))',
-          foreground: 'hsl(var(--sidebar-foreground))',
-          primary: 'hsl(var(--sidebar-primary))',
-          'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
-          accent: 'hsl(var(--sidebar-accent))',
-          'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-          border: 'hsl(var(--sidebar-border))',
-          ring: 'hsl(var(--sidebar-ring))',
-        },
-      },
-      fontFamily: {
-        mono: ['var(--font-geist-mono)'],
-        sans: ['var(--font-geist-sans)'],
-      },
-      keyframes: {
-        'accordion-down': {
-          from: {
-            height: '0',
-          },
-          to: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-        },
-        'accordion-up': {
-          from: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-          to: {
-            height: '0',
-          },
-        },
-        transitionTimingFunction: {
-          'custom-bezier': 'cubic-bezier(0.77, 0, 0.175, 1)',
-        },
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            '--tw-prose-body': 'var(--text)',
-            '--tw-prose-headings': 'var(--text)',
-            h1: {
-              fontWeight: 'normal',
-              marginBottom: '0.25em',
-            },
-          },
-        },
-        base: {
-          css: {
-            h1: {
-              fontSize: '2.5rem',
-            },
-            h2: {
-              fontSize: '1.25rem',
-              fontWeight: 600,
-            },
-          },
-        },
-        md: {
-          css: {
-            h1: {
-              fontSize: '3.5rem',
-            },
-            h2: {
-              fontSize: '1.5rem',
-            },
-          },
-        },
-      },
-    },
+  	container: {
+  		center: true,
+  		padding: {
+  			'2xl': '2rem',
+  			DEFAULT: '1rem',
+  			lg: '2rem',
+  			md: '2rem',
+  			sm: '1rem',
+  			xl: '2rem'
+  		},
+  		screens: {
+  			'2xl': '86rem',
+  			lg: '64rem',
+  			md: '48rem',
+  			sm: '40rem',
+  			xl: '80rem'
+  		}
+  	},
+  	extend: {
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			border: 'var(--border)',
+  			input: 'var(--input)',
+  			ring: 'var(--ring)',
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)',
+  			primary: {
+  				DEFAULT: 'var(--primary)',
+  				foreground: 'var(--primary-foreground)'
+  			},
+  			secondary: {
+  				DEFAULT: 'var(--secondary)',
+  				foreground: 'var(--secondary-foreground)'
+  			},
+  			tertiary: {
+  				DEFAULT: 'var(--tertiary)',
+  				foreground: 'var(--tertiary-foreground)'
+  			},
+  			yellow: {
+  				DEFAULT: 'var(--yellow)',
+  				foreground: 'var(--yellow-foreground)'
+  			},
+  			darkyellow: {
+  				DEFAULT: 'var(--darkyellow)',
+  				foreground: 'var(--darkyellow-foreground)'
+  			},
+  			purple: {
+  				DEFAULT: 'var(--purple)',
+  				foreground: 'var(--purple-foreground)'
+  			},
+  			blue: {
+  				DEFAULT: 'var(--blue)',
+  				foreground: 'var(--blue-foreground)'
+  			},
+  			darkbrown: {
+  				DEFAULT: 'var(--darkbrown)',
+  				foreground: 'var(--darkbrown-foreground)'
+  			},
+  			darkgreen: {
+  				DEFAULT: 'var(--darkgreen)',
+  				foreground: 'var(--darkgreen-foreground)'
+  			},
+  			destructive: {
+  				DEFAULT: 'var(--destructive)',
+  				foreground: 'var(--destructive-foreground)'
+  			},
+  			muted: {
+  				DEFAULT: 'var(--muted)',
+  				foreground: 'var(--muted-foreground)'
+  			},
+  			accent: {
+  				DEFAULT: 'var(--accent)',
+  				foreground: 'var(--accent-foreground)'
+  			},
+  			popover: {
+  				DEFAULT: 'var(--popover)',
+  				foreground: 'var(--popover-foreground)'
+  			},
+  			card: {
+  				DEFAULT: 'var(--card)',
+  				foreground: 'var(--card-foreground)'
+  			},
+  			success: {
+  				DEFAULT: 'var(--success)',
+  				foreground: 'var(--success-foreground)'
+  			},
+  			warning: 'var(--warning)',
+  			error: 'var(--error)',
+  			'chart-1': 'var(--chart-1)',
+  			'chart-2': 'var(--chart-2)',
+  			'chart-3': 'var(--chart-3)',
+  			'chart-4': 'var(--chart-4)',
+  			'chart-5': 'var(--chart-5)',
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		},
+  		fontFamily: {
+  			mono: [
+  				'var(--font-geist-mono)'
+  			],
+  			sans: [
+  				'var(--font-geist-sans)'
+  			]
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			},
+  			transitionTimingFunction: {
+  				'custom-bezier': 'cubic-bezier(0.77, 0, 0.175, 1)'
+  			}
+  		},
+  		typography: {
+  			DEFAULT: {
+  				css: {
+  					'--tw-prose-body': 'var(--text)',
+  					'--tw-prose-headings': 'var(--text)',
+  					h1: {
+  						fontWeight: 'normal',
+  						marginBottom: '0.25em'
+  					}
+  				}
+  			},
+  			base: {
+  				css: {
+  					h1: {
+  						fontSize: '2.5rem'
+  					},
+  					h2: {
+  						fontSize: '1.25rem',
+  						fontWeight: 600
+  					}
+  				}
+  			},
+  			md: {
+  				css: {
+  					h1: {
+  						fontSize: '3.5rem'
+  					},
+  					h2: {
+  						fontSize: '1.5rem'
+  					}
+  				}
+  			}
+  		}
+  	}
   },
 }
 
